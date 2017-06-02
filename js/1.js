57b64ff1cf607c74ef91a2845bfbb69ed2c1f44b
lovesispham <wangjidw@gmail.com>
2017-06-02 20:41:35 +0700

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,33 @@
+﻿$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText : ["",""],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+// $(function(){
+//     $( ".level0 li .plus" ).click(function() {
+//     $(this).parent().find( ".dropdown-content" ).slideToggle(function() {
+//       $(this).next().stop(true).toggleClass('open', $(this).is(":visible"));
+//     });
+//   });
+// });
+
+$(function(){
+   $('.level0 > li').click(function() {
+    $('.level0 > li').not(this).find('.dropdown-content').hide();
+    $(this).find('.dropdown-content').stop(true, true).slideToggle(400);
+    
+});
+});
